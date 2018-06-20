deeaa187a9f7946bbfba4f4b2a12b88a46db5181
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-20 11:09:05 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -15,24 +15,33 @@ public class App {
 		    String str2 = String.valueOf(input.charAt(i));
 		    String outstr = "";
 		    switch(str2){
-		    case "A" : outstr = "0";
-		    case "B" : outstr =  "1";
-		    case "C" : outstr =  "2";
-		    case "D" : outstr =  "3";
-		    case "E" : outstr =  "4";
-		    case "F" : outstr =  "5";
-		    case "G" : outstr =  "6";
-		    case "H" : outstr =  "7";
-		    case "I" : outstr =  "8";
-		    case "a" : outstr =  "0";
-		    case "b" : outstr =  "1";
-		    case "c" : outstr =  "2";
-		    case "d" : outstr =  "3";
-		    case "e" : outstr =  "4";
-		    case "f" : outstr =  "5";
-		    case "g" : outstr =  "6";
-		    case "h" : outstr =  "7";
-		    case "i" : outstr =  "8";
+		    case "A" :
+		    	outstr = "0";
+		    	break;
+		    case "B" :
+		    	outstr =  "1";
+		    	break;
+		    case "C" :
+		    	outstr =  "2";
+		    	break;
+		    case "D" :
+		    	outstr =  "3";
+		    	break;
+		    case "E" :
+		    	outstr =  "4";
+		    	break;
+		    case "F" :
+		    	outstr =  "5";
+		    	break;
+		    case "G" :
+		    	outstr =  "6";
+		    	break;
+		    case "H" :
+		    	outstr =  "7";
+		    	break;
+		    case "I" :
+		    	outstr =  "8";
+		    	break;
 		    }
 
 		    // 配列に順番に格納する

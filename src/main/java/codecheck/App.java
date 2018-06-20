870e45deb120ef1eaf1b519f9ab0caa5985bf36f
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-20 12:00:55 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -8,27 +8,43 @@ public class App {
 		String subcmd = args[0];
 		String input = args[1];
 
-		StringBuilder str = new StringBuilder();;
-		double jou = input.length()-1;
-		double output = 0;
+		if(subcmd == "decode"){
+			double jou = input.length()-1;
+			double out_decode = 0;
 
-		for (int i = 0; i < input.length(); i++) {
+			for (int i = 0; i < input.length(); i++) {
 
-			String str2 = String.valueOf(input.charAt(i));
-		    int re_in8 = Integer.parseInt(return_int8(str2));
+				String str2 = String.valueOf(input.charAt(i));
+			    int re_int8 = Integer.parseInt(return_int8(str2));
 
 
-		    int num = 0;
-		    if(jou == 0){
-		    	num = re_in8;
-		    }else{
-		    	num = (int) (re_in8 * Math.pow(9, jou));
+			    int num = 0;
+			    if(jou == 0){
+			    	num = re_int8;
+			    }else{
+			    	num = (int) (re_int8 * Math.pow(9, jou));
+			    }
+
+			    out_decode += num;
+			    jou--;
+			}
+		    System.out.println(out_decode);
+
+		}else if(subcmd == "encode"){
+
+		    int input_i = Integer.parseInt(input);
+		    String oct = Integer.toOctalString(input_i);
+		    StringBuilder out_encode = new StringBuilder();
+
+		    for (int i = 0; i < input.length(); i++) {
+
+				String str2 = String.valueOf(input.charAt(i));
+				out_encode.append(return_A(str2));
 		    }
-
-		    output += num;
-		    jou--;
+		    System.out.println(out_encode.toString());
 		}
-	    System.out.println(output);
+
+
 	}
 
 	private static String return_int8(String input_A) {
@@ -64,5 +80,40 @@ public class App {
 	    	break;
 	    }
 	    return return_str8;
+	}
+
+	private static String return_A(String input_i) {
+
+		String return_strA = "";
+	    switch(input_i){
+	    case "0" :
+	    	return_strA = "A";
+	    	break;
+		case "1" :
+		    return_strA = "B";
+		    break;
+		case "2" :
+		    return_strA = "C";
+		    break;
+		case "3" :
+		    return_strA = "D";
+		    break;
+		case "4" :
+		    return_strA = "E";
+		    break;
+		case "5" :
+		    return_strA = "F";
+		    break;
+		case "6" :
+		    return_strA = "G";
+		    break;
+		case "7" :
+		    return_strA = "H";
+		    break;
+		case "8" :
+		    return_strA = "I";
+		    break;
+	    }
+	    return return_strA;
 	}
 }

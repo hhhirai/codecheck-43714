b53cd22a64ad1177aab497e3d5a26620b30f6b1d
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-20 11:03:51 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -13,31 +13,31 @@ public class App {
 		for (int i = 0; i < input.length(); i++) {
 		    // strの先頭から1文字ずつString型にして取り出す
 		    String str2 = String.valueOf(input.charAt(i));
-
+		    String outstr = "";
 		    switch(str2){
-		    case "A" : str2 = "0";
-		    case "B" : str2 =  "1";
-		    case "C" : str2 =  "2";
-		    case "D" : str2 =  "3";
-		    case "E" : str2 =  "4";
-		    case "F" : str2 =  "5";
-		    case "G" : str2 =  "6";
-		    case "H" : str2 =  "7";
-		    case "I" : str2 =  "8";
-		    case "a" : str2 =  "0";
-		    case "b" : str2 =  "1";
-		    case "c" : str2 =  "2";
-		    case "d" : str2 =  "3";
-		    case "e" : str2 =  "4";
-		    case "f" : str2 =  "5";
-		    case "g" : str2 =  "6";
-		    case "h" : str2 =  "7";
-		    case "i" : str2 =  "8";
+		    case "A" : outstr = "0";
+		    case "B" : outstr =  "1";
+		    case "C" : outstr =  "2";
+		    case "D" : outstr =  "3";
+		    case "E" : outstr =  "4";
+		    case "F" : outstr =  "5";
+		    case "G" : outstr =  "6";
+		    case "H" : outstr =  "7";
+		    case "I" : outstr =  "8";
+		    case "a" : outstr =  "0";
+		    case "b" : outstr =  "1";
+		    case "c" : outstr =  "2";
+		    case "d" : outstr =  "3";
+		    case "e" : outstr =  "4";
+		    case "f" : outstr =  "5";
+		    case "g" : outstr =  "6";
+		    case "h" : outstr =  "7";
+		    case "i" : outstr =  "8";
 		    }
 
 		    // 配列に順番に格納する
 //		    str.append(re_in8);
-		    System.out.println(str2);
+		    System.out.println(str2 + ":" + outstr);
 		}
 	}
 
